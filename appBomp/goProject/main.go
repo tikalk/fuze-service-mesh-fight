bb80656cabe3648d6d918339e9187148a7c0a03d
Evyatar <evyatar@tikalk.com>
2020-02-17 13:07:45 +0200

    another implementation for HTTP server

--- appBomp/goProject/main.go
+++ appBomp/goProject/main.go
@@ -63,6 +63,71 @@ func params(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf(`{"userID": %d, "commentID": %d, "location": "%s" }`, userID, commentID, location)))
 }
 
+func startSending1(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	timeInSeconds := -1
+	var err error
+
+	pathParams := mux.Vars(r)
+	if val, ok := pathParams["timeInSeconds"]; ok {
+		timeInSeconds, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message": "timeInSeconds must be a number"}`))
+			return
+		}
+	}
+
+	concurrentThreads := -1
+	if val, ok := pathParams["concurrentThreads"]; ok {
+		concurrentThreads, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message": "need a number for commentID"}`))
+			return
+		}
+	}
+
+	timeInSeconds = timeInSeconds + 1
+	concurrentThreads = concurrentThreads + 1
+
+	w.Write([]byte(fmt.Sprintf(`{"timeInSeconds": %d, "concurrentThreads": %d, " OK" }`, timeInSeconds, concurrentThreads)))
+}
+
+func startSending2(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	//timeInSeconds = 60
+	//concurrentThreads := 1
+	query := r.URL.Query()
+	timeInSeconds, ok1 := strconv.Atoi(query.Get("timeInSeconds"))
+	concurrentThreads, ok2 := strconv.Atoi(query.Get("concurrentThreads"))
+
+	println(ok1)
+	println(ok2)
+	println(timeInSeconds)
+	println(concurrentThreads)
+
+	go startSendingRequests(timeInSeconds, concurrentThreads)
+
+	w.Write([]byte(fmt.Sprintf(`{"timeInSeconds": %d, "concurrentThreads": %d, " OK" }`, timeInSeconds, concurrentThreads)))
+
+}
+
+func startSendingRequests(timeInSeconds int, concurrentThreads int) {
+	println("Starting ...")
+	busyWait()
+	println("completed!")
+}
+
+func busyWait() {
+	for a := 0; a <= 10000-1; a++ {
+		for c := 0; c <= 1000000-1; c++ {
+			b := 0
+			b = b + 1
+		}
+	}
+}
+
 /*******************************
 Clear is Better than Clever
 Rob Pike
@@ -77,6 +142,8 @@ func main() {
 	api.HandleFunc("", delete).Methods(http.MethodDelete)
 
 	api.HandleFunc("/user/{userID}/comment/{commentID}", params).Methods(http.MethodGet)
+	api.HandleFunc("/start/{timeInSeconds}/{concurrentThreads}", startSending1).Methods(http.MethodGet)
+	api.HandleFunc("/start", startSending2).Methods(http.MethodGet) // ?timeInSeconds=3&concurrentThreads=4
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
