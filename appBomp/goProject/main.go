ce9b0c638bb77cb7356c514b46a2c164dd234a4c
Evyatar <evyatar@tikalk.com>
2020-02-17 12:24:59 +0200

    another implementation for HTTP server

--- appBomp/goProject/main.go
+++ appBomp/goProject/main.go
@@ -3,23 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type server struct{}
+//type server struct{}
 
 // satisfy the Handler interface
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
 	case "GET":
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "get called"}`))
+		w.Write([]byte(`{"message": "##get called"}`))
 	case "POST":
 		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message": "post called"}`))
+		w.Write([]byte(`{"message": "##post called"}`))
 	case "PUT":
 		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(`{"message": "put called"}`))
+		w.Write([]byte(`{"message": "##put called"}`))
 	case "DELETE":
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"message": "delete called"}`))
@@ -29,15 +31,8 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s *server) ServeHTTPv1(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message": "hello world"}`))
-}
-
 func main() {
-	s := &server{}
-	http.Handle("/", s)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
